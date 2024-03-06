c2769d6d636733e97d8895cc52d962b43623ac18
Eeza <eesaard@gmail.com>
2024-03-06 19:37:39 +0530

    changes

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import datetime
 import cv2
-import csv
 import os
-from google.oauth2 import service_account
-import gspread
+import pyrebase
+import time
+import tempfile
 
-# Set up Google Sheets API credentials
-credentials = service_account.Credentials.from_service_account_file('year-project.json', scopes=['https://www.googleapis.com/auth/spreadsheets'])
+firebaseConfig = {
+    "apiKey": "AIzaSyC0k6uY_XxRYTsjRqddDqujPf4lKacp2fo",
+    "authDomain": "shahil-ai-project.firebaseapp.com",
+    "databaseURL": "https://shahil-ai-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "shahil-ai-project",
+    "storageBucket": "shahil-ai-project.appspot.com",
+    "messagingSenderId": "799127360843",
+    "appId": "1:799127360843:web:f707a808c47b6d4032c4ce",
+}
 
-# Open the Google Sheet
-gc = gspread.authorize(credentials)
-sheet = gc.open('Your Google Sheet Name').sheet1  # Replace with your sheet name
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage = firebase.storage()
+db = firebase.database()
 
 thres = 0.555
 
@@ -20,12 +28,12 @@ cap.set(4, 720)
 cap.set(10, 70)
 
 classNames = []
-classFile = 'coco.names'
-with open(classFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+classFile = "coco.names"
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
 
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
+configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "frozen_inference_graph.pb"
 
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
 net.setInputSize(320, 320)
@@ -33,71 +41,123 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-csv_file = 'object_detection_data.csv'
-image_folder = 'captured_objects'
-
-# Create the image folder if it doesn't exist
+image_folder = "captured_objects"
 os.makedirs(image_folder, exist_ok=True)
 
-with open(csv_file, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Category', 'Date', 'Time', 'Confidence', 'Image Path'])
 
-    while True:
-        success, img = cap.read()
+def generate_document_key():
+    return "{}".format(int(time.time() * 1000))
 
-        if not success:
-            break
 
-        classIds, confs, bbox = net.detect(img, confThreshold=thres)
+while True:
+    success, img = cap.read()
 
-        if len(classIds) != 0:
-            for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                timestamp = datetime.datetime.now()
+    if not success:
+        break
 
-                # Extract date and time components
-                date = timestamp.date()
-                time = timestamp.strftime("%H:%M:%S")
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
 
-                if classId > 0 and classId <= len(classNames):
-                    class_name = classNames[classId - 1].lower()
-                else:
-                    class_name = "unknown"
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            timestamp = datetime.datetime.now()
+            date = timestamp.date()
+            time_str = timestamp.strftime("%H:%M:%S")
 
-                # Initialize the image path variable
-                object_image_path = ""
+            if classId > 0 and classId <= len(classNames):
+                class_name = classNames[classId - 1].lower()
+            else:
+                class_name = "unknown"
 
-                # Check if the detected object is a person or an animal
-                if class_name == "person" or class_name in ['cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe']:
-                    object_category = "person" if class_name == "person" else "animal"
+            object_image_path = ""
+            if class_name == "person" or class_name in [
+                "cat",
+                "dog",
+                "horse",
+                "sheep",
+                "cow",
+                "elephant",
+                "bear",
+                "zebra",
+                "giraffe",
+            ]:
+                object_category = "person" if class_name == "person" else "animal"
+                object_image_path = os.path.join(
+                    image_folder,
+                    f'{object_category}_{timestamp.strftime("%Y%m%d%H%M%S")}.jpg',
+                )
+                cv2.imwrite(
+                    object_image_path,
+                    img[box[1] : box[1] + box[3], box[0] : box[0] + box[2]],
+                )
+            elif class_name in [
+                "bicycle",
+                "car",
+                "motorcycle",
+                "airplane",
+                "bus",
+                "train",
+                "truck",
+                "boat",
+            ]:
+                object_category = "vehicle"
+            else:
+                object_category = "object"
 
-                    # Capture and save the image
-                    object_image_path = os.path.join(image_folder, f'{object_category}_{timestamp.strftime("%Y%m%d%H%M%S")}.jpg')
-                    cv2.imwrite(object_image_path, img[box[1]:box[1] + box[3], box[0]:box[0] + box[2]])
-                elif class_name in ['bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat']:
-                    object_category = "vehicle"
-                else:
-                    object_category = "object"
+            if (
+                object_category != "object" and confidence > 0.7
+            ): 
+                with open(object_image_path, "rb") as image_file:
+                    temp_local_filename = tempfile.mktemp()
+                    with open(temp_local_filename, "wb") as temp_local_file:
+                        temp_local_file.write(image_file.read())
 
-                # Check if the object category is not "object" before writing to CSV and Google Sheet
-                if object_category != "object":
-                    object_data = [object_category, date, time, round(confidence * 100, 2), object_image_path]
-                    # Write data to CSV
-                    writer.writerow(object_data)
+                    storage.child("images/" + os.path.basename(object_image_path)).put(
+                        temp_local_filename
+                    )
+                    image_url = storage.child(
+                        "images/" + os.path.basename(object_image_path)
+                    ).get_url(None)
 
-                    # Write data to Google Sheet
-                    sheet.append_row(object_data)
+                # Create a specific document key for each entry
+                document_key = generate_document_key()
 
-                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                cv2.putText(img, object_category.upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (0, 255, 0), 2)
-                cv2.putText(img, f"Confidence: {round(confidence * 100, 2)}%", (box[0] + 10, box[1] + 60),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                # Create a dictionary for the object data
+                object_data = {
+                    "id": document_key,
+                    "category": object_category,
+                    "date": str(date),
+                    "time": time_str,
+                    "confidence": round(float(confidence) * 100, 2),
+                    "image_url": image_url,
+                }
 
-        cv2.imshow("Object Detection", img)
+                # Add data to Firestore with the specific document key
+                db.child("object_detection_data").child(document_key).set(object_data)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(
+                img,
+                object_category.upper(),
+                (box[0] + 10, box[1] + 30),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2,
+            )
+            cv2.putText(
+                img,
+                f"Confidence: {round(float(confidence) * 100, 2)}%",
+                (box[0] + 10, box[1] + 60),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2,
+            )
+
+    cv2.imshow("Object Detection", img)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
